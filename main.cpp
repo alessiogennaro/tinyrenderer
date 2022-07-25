221d1d7b63e216df488207f34379016b11f29947
Alessio9927 <alessiogennaro9927@gmail.com>
2022-07-25 08:52:14 +0200

    Added light and shades

--- main.cpp
+++ main.cpp
@@ -139,9 +139,23 @@ int main(int argc, char** argv) {
 	for (int i=0; i<model->nfaces(); i++) {
         std::vector<int> face = model->face(i);
         std::array<Vec3f, 3> pts {};
+		std::array<Vec3f, 3> world_coords {};
         
-		for (int i = 0; i < 3; i++) pts[i] = world2screen(model->vert( face[i] ));
-        triangle(pts, z_buffer, canvas, TGAColor(RAND_COLOR, RAND_COLOR, RAND_COLOR, 255));
+		for (int j = 0; j < 3; j++) {
+			pts[j] = world2screen(model->vert( face[j] ));
+			world_coords[j] = model->vert( face[j] );
+		}
+		Vec3f n = (world_coords[2] - world_coords[0])^(world_coords[1] - world_coords[0]);
+		n.normalize();
+
+		float intensity = n * light_vec;
+
+		if (intensity > 0.) {
+			int color = intensity * 255;
+			triangle(pts, z_buffer, canvas, TGAColor(color, color, color, 255));
+		}
+
+		// triangle(pts, z_buffer, canvas, TGAColor(RAND_COLOR, RAND_COLOR, RAND_COLOR, 255));
     }
 
 	delete[] z_buffer;
